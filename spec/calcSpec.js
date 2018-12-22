40c0bf6d07e8c5039e3ccfa5c8a56bad9c66af61
Laurence <loughranlaurence@gmail.com>
2018-12-22 19:54:01 +0000

    Drink Game

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,10 +1,42 @@
-describe("calculator", function(){
-    describe("minus tests", function(){
-        it("should return 10", function(){
-            expect(minus(20, 10)).toBe(10);
+describe("whatCanIDrink", function(){
+    beforeEach(function(){
+        drink = new whatCanIDrink();
+    });
+    
+    describe("check age", function() {
+        it("all good", function(){
+            expect(whatCanIDrink).toBeDefined();
         });
-        it("should return 50", function(){
-            expect(minus(100, 50)).toBe(50);
-        })
+        it("should return drink toddy(13)", function(){
+            var result = whatCanIDrink(13);
+            expect(result).toBe("Drink Toddy!");
+        });
+        
+        it("should return drink coke(17)", function(){
+            var result = whatCanIDrink(17)
+            expect(result).toBe("Drink Coke!");
+        
+        });
+        it("should return drink beer(18)", function(){
+            var result = whatCanIDrink(18)
+            expect(result).toBe("Drink Beer!");
+    
+        });
+        it("should return drink more beer(20)", function(){
+            var result = whatCanIDrink(20)
+            expect(result).toBe("Drink Beer!");
+        });
+        it("should return drink whiskey(30)", function(){
+            var result = whatCanIDrink(30)
+            expect(result).toBe("Drink Whiskey!");
+        });
+        it("should return no drink when called as whatCanIDrink(140)", function(){
+            var result = whatCanIDrink(140)
+            expect(result).toBe("Incorrect age input!");
+        });
+        it("should return drink when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(10).indexOf("Drink");
+            expect(result).not.toEqual(-1);
+        });
     });
 });
